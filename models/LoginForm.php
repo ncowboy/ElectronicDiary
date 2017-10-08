e57ada6425e5e7359575862dbe6e5ec2c8f835a2
ncowboy <latentpickuper@gmail.com>
2017-10-08 18:39:33 +0300

    Title на главной, вью для login form

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -16,18 +16,26 @@ class LoginForm extends Model
     public $username;
     public $password;
     public $rememberMe = true;
-
     private $_user = false;
 
 
-    /**
-     * @return array the validation rules.
-     */
+   public function attributeLabels()
+    {
+        return [
+            'username' => 'Логин',
+            'password' => 'Пароль',
+            'rememberMe' => 'Запомнить меня',
+        ];
+    } 
+    
+    
+    
+    
     public function rules()
     {
         return [
             // username and password are both required
-            [['username', 'password'], 'required'],
+            [['username', 'password'], 'required', 'message' => 'Поле не может быть пустым'],
             // rememberMe must be a boolean value
             ['rememberMe', 'boolean'],
             // password is validated by validatePassword()
--- views/site/index.php
+++ views/site/index.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Курсы по подготовке к ЕГЭ и ОГЭ в Москве - Merlin';
 ?>
 <div class="site-index">
 
