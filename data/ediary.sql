9489707bb227f4cb1864b04ada8978775d0f1f11
ncowboy <latentpickuper@gmail.com>
2017-11-12 16:25:37 +0300

    дамп

--- data/ediary.sql
+++ data/ediary.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 17 2017 г., 19:06
--- Версия сервера: 5.7.19
--- Версия PHP: 7.1.7
+-- Время создания: Ноя 12 2017 г., 16:24
+-- Версия сервера: 5.6.37
+-- Версия PHP: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -57,7 +57,7 @@ CREATE TABLE `groups` (
   `building_id` int(11) NOT NULL,
   `subject_id` int(11) NOT NULL,
   `group_type_id` int(11) NOT NULL,
-  `teacher_id` int(10) NOT NULL DEFAULT '0'
+  `teacher_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -65,11 +65,11 @@ CREATE TABLE `groups` (
 --
 
 INSERT INTO `groups` (`id`, `building_id`, `subject_id`, `group_type_id`, `teacher_id`) VALUES
-(2, 3, 3, 2, 1),
-(3, 2, 2, 2, 3),
-(5, 2, 1, 1, 3),
-(7, 1, 1, 1, 1),
-(8, 1, 2, 3, 0);
+(3, 2, 2, 2, 1),
+(5, 2, 1, 1, 1),
+(7, 1, 1, 1, 2),
+(8, 1, 1, 2, NULL),
+(10, 3, 1, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -104,17 +104,18 @@ CREATE TABLE `lessons` (
   `datetime` datetime DEFAULT NULL,
   `theme` varchar(256) DEFAULT NULL,
   `group_id` int(11) DEFAULT NULL,
-  `subject_id` int(11) NOT NULL
+  `comment` varchar(1028) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `lessons`
 --
 
-INSERT INTO `lessons` (`id`, `datetime`, `theme`, `group_id`, `subject_id`) VALUES
-(3, '2017-10-24 20:00:00', 'Глаголы', 2, 2),
-(4, '2017-10-31 18:00:00', 'Фонетика', 2, 2),
-(5, '2017-10-24 20:00:00', 'testтема', 7, 1);
+INSERT INTO `lessons` (`id`, `datetime`, `theme`, `group_id`, `comment`) VALUES
+(5, '2018-01-19 14:50:00', 'testтема', 7, ''),
+(6, '2017-10-28 14:50:00', 'Поэзия серебряного века', 5, ''),
+(7, '2017-10-20 13:05:00', 'testтема', 3, ''),
+(15, '2017-10-24 20:00:00', 'testтема11111', 10, 'drthrhtrh');
 
 -- --------------------------------------------------------
 
@@ -156,10 +157,10 @@ INSERT INTO `migration` (`version`, `apply_time`) VALUES
 CREATE TABLE `students` (
   `id` int(5) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `group_id` int(5) DEFAULT NULL,
   `phone_number` varchar(20) DEFAULT NULL,
   `parents_name` varchar(50) DEFAULT NULL,
   `parents_number` varchar(20) DEFAULT NULL,
+  `parents_email` varchar(100) NOT NULL,
   `birth` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -167,9 +168,12 @@ CREATE TABLE `students` (
 -- Дамп данных таблицы `students`
 --
 
-INSERT INTO `students` (`id`, `user_id`, `group_id`, `phone_number`, `parents_name`, `parents_number`, `birth`) VALUES
-(1, 4, NULL, '+1 (111) 111-11-11', 'Мария Ивановна Петрова', '+1 (111) 111-11-11', '1983-11-09'),
-(2, 5, 2, '+7(903) 155-55-45', 'Тестстудентовна', '+7(905)125-28-41', '1983-11-09');
+INSERT INTO `students` (`id`, `user_id`, `phone_number`, `parents_name`, `parents_number`, `parents_email`, `birth`) VALUES
+(1, 4, '+7 (903) 588-42-07', 'Мария Ивановна Петрова', '+7-111-11-11', 'mail@mail.me', '1983-12-07'),
+(2, 5, '+7(903) 155-15-20', 'Денисова Анна Евгеньевна', '+7(905)125-20-55', '', '1983-11-09'),
+(19, 24, '89765678990', 'Мама', '87686545656', '', '1998-10-09'),
+(20, 25, '89034567890', 'Папа', '89045678929', '', '1997-10-16'),
+(21, 26, '89076789098', 'Мама', '89165434567', '', '2000-08-25');
 
 -- --------------------------------------------------------
 
@@ -178,20 +182,35 @@ INSERT INTO `students` (`id`, `user_id`, `group_id`, `phone_number`, `parents_na
 --
 
 CREATE TABLE `students_in_group` (
-  `student_id` int(11) NOT NULL,
-  `group_id` int(11) NOT NULL
+  `group_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `students_in_group`
 --
 
-INSERT INTO `students_in_group` (`student_id`, `group_id`) VALUES
-(1, 2),
-(2, 2),
-(1, 3),
-(1, 7),
-(2, 7);
+INSERT INTO `students_in_group` (`group_id`, `student_id`) VALUES
+(3, 1),
+(3, 2),
+(3, 19),
+(3, 20),
+(3, 21),
+(5, 1),
+(5, 2),
+(5, 19),
+(5, 20),
+(5, 21),
+(7, 1),
+(7, 2),
+(7, 19),
+(7, 20),
+(7, 21),
+(10, 1),
+(10, 2),
+(10, 19),
+(10, 20),
+(10, 21);
 
 -- --------------------------------------------------------
 
@@ -202,20 +221,29 @@ INSERT INTO `students_in_group` (`student_id`, `group_id`) VALUES
 CREATE TABLE `students_in_lesson` (
   `lesson_id` int(11) NOT NULL,
   `student_id` int(11) NOT NULL,
-  `attendance` tinyint(1) DEFAULT NULL,
+  `attendance` tinyint(1) NOT NULL DEFAULT '0',
   `mark_work_at_lesson` tinyint(4) DEFAULT NULL,
   `mark_homework` tinyint(4) DEFAULT NULL,
-  `mark_dictation` tinyint(4) DEFAULT NULL
+  `mark_dictation` tinyint(4) DEFAULT NULL,
+  `comment` varchar(1028) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `students_in_lesson`
 --
 
-INSERT INTO `students_in_lesson` (`lesson_id`, `student_id`, `attendance`, `mark_work_at_lesson`, `mark_homework`, `mark_dictation`) VALUES
-(3, 1, 1, NULL, NULL, NULL),
-(3, 2, NULL, NULL, NULL, NULL),
-(5, 1, NULL, NULL, NULL, NULL);
+INSERT INTO `students_in_lesson` (`lesson_id`, `student_id`, `attendance`, `mark_work_at_lesson`, `mark_homework`, `mark_dictation`, `comment`) VALUES
+(5, 1, 0, NULL, NULL, NULL, ''),
+(7, 1, 1, 4, 5, 3, ''),
+(7, 2, 1, 5, 3, 4, ''),
+(7, 19, 1, 1, 1, 5, ''),
+(7, 20, 1, 5, 1, 5, ''),
+(7, 21, 1, 1, 1, 3, ''),
+(15, 1, 1, 5, NULL, NULL, 'Все хорошо'),
+(15, 2, 1, 5, 3, NULL, ''),
+(15, 19, 1, 4, NULL, NULL, ''),
+(15, 20, 1, 4, NULL, NULL, ''),
+(15, 21, 1, 4, NULL, NULL, '');
 
 -- --------------------------------------------------------
 
@@ -246,18 +274,19 @@ INSERT INTO `subjects` (`id`, `name`, `alias`) VALUES
 
 CREATE TABLE `teachers` (
   `id` int(5) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `specialization` varchar(50) DEFAULT NULL
+  `user_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `specialization` varchar(50) DEFAULT NULL,
+  `group_id` int(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `teachers`
 --
 
-INSERT INTO `teachers` (`id`, `user_id`, `specialization`) VALUES
-(0, NULL, ''),
-(1, 8, 'Математика'),
-(3, 9, 'Информатика');
+INSERT INTO `teachers` (`id`, `user_id`, `name`, `specialization`, `group_id`) VALUES
+(1, 22, '', 'Информатика', NULL),
+(2, 23, '', 'Программирование', NULL);
 
 -- --------------------------------------------------------
 
@@ -283,11 +312,11 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
+  `email` varchar(20) NOT NULL,
   `surname` varchar(20) NOT NULL,
   `name` varchar(20) NOT NULL,
   `patronymic` varchar(20) NOT NULL,
-  `user_role` int(11) NOT NULL DEFAULT '0',
+  `user_role` int(11) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -300,10 +329,13 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`, `surname`, `name`, `
 (1, 'admin', 'f6fdffe48c908deb0f4c3bd36c032e72', 'admin@admin.ad', 'Админов', 'Админ', 'Админович', 1, NULL, '2017-09-25 15:06:37'),
 (2, 'super', '727dfbdc1a4ee249f3f08c247a5669d5', 'super@super.su', 'Супер', 'Супер', 'Суперович', 2, '2017-09-25 14:57:17', '2017-09-25 14:57:17'),
 (3, 'curator', '8a08422b4ca8a0ab0f443628d627c96b', 'curator@curator.cu', 'Кураторов', 'Куратор', 'Кураторович', 3, '2017-09-25 14:57:57', '2017-09-25 14:57:57'),
-(4, 'student', '4369c4fb2a7b43d39634be616efdd8a8', 'student@student.st', 'Петрова ', 'Анна ', 'Николаевна', 5, '2017-09-25 14:58:43', '2017-10-09 18:19:20'),
-(5, 'student1', '4369c4fb2a7b43d39634be616efdd8a8', 'student@student.st', 'Денисов', 'Артем ', 'Валерьевич', 5, '2017-09-25 14:58:43', '2017-10-10 17:52:08'),
-(8, 'teacher1', '92d2ae7c028098db472bcf582f212b78', 'teacher@teacher.te', 'Гусынин', 'Владислав ', 'Игоревич', 4, '2017-10-10 18:32:17', '2017-10-10 18:35:43'),
-(9, 'teacher2', 'ccffb0bb993eeb79059b31e1611ec353', 'teacher2@teacher.te', 'Петрова', 'Анна', 'Юревна', 4, '2017-10-10 18:35:18', '2017-10-10 18:35:18');
+(4, 'student', 'abd842186026b1110b67d3e39a96e809', 'student@student.st', 'Петрова ', 'Анна ', 'Николаевна', 5, '2017-09-25 14:58:43', '2017-10-03 18:36:47'),
+(5, 'student1', 'abd842186026b1110b67d3e39a96e809', 'student@student.st', 'Денисов', 'Артем ', 'Валерьевич', 5, '2017-09-25 14:58:43', '2017-10-03 18:37:23'),
+(22, 'vladgus', '1b12491c473c1e74bd068e65c5842f17', 'gusynin.vlad@gmail.c', 'Гусынин', 'Владислав', 'Игоревич', 4, '2017-10-14 15:23:24', '2017-10-14 15:23:24'),
+(23, 'iborisov', '2a803100dee39458287b9c0a04503eb7', 'latentpickuper@gmail', 'Борисов', 'Игорь', 'Владимирович', 4, '2017-10-14 15:24:21', '2017-10-14 15:24:21'),
+(24, 'student2', '213ee683360d88249109c2f92789dbc3', 'polina@gmail.com', 'Гагарина', 'Полина', 'Сергеевна', 5, '2017-10-15 03:22:19', '2017-10-15 03:22:19'),
+(25, 'student3', 'f6fdffe48c908deb0f4c3bd36c032e72', 'arkadiev@yahoo.com', 'Аркадьев ', 'Петр ', 'Сергеевич', 5, '2017-10-15 03:23:58', '2017-10-15 03:23:58'),
+(26, 'admin', 'f6fdffe48c908deb0f4c3bd36c032e72', 'hmel89@mail.ru', 'Хмелева ', 'Наталия ', 'Степановна', 5, '2017-10-15 03:25:04', '2017-10-15 03:25:04');
 
 -- --------------------------------------------------------
 
@@ -322,7 +354,6 @@ CREATE TABLE `user_roles` (
 --
 
 INSERT INTO `user_roles` (`id_user_role`, `role_name`, `role_alias`) VALUES
-(0, 'not selected', 'Роль не выбрана'),
 (1, 'Administrator', 'Администратор'),
 (2, 'Superviser', 'Супервайзер'),
 (3, 'Curator', 'Куратор'),
@@ -346,8 +377,7 @@ ALTER TABLE `groups`
   ADD PRIMARY KEY (`id`),
   ADD KEY `building_ibfk_1` (`building_id`),
   ADD KEY `subjects_ibfk_1` (`subject_id`),
-  ADD KEY `grouptype_ibfk_1` (`group_type_id`),
-  ADD KEY `teacher_id` (`teacher_id`);
+  ADD KEY `grouptype_ibfk_1` (`group_type_id`);
 
 --
 -- Индексы таблицы `group_types`
@@ -360,8 +390,7 @@ ALTER TABLE `group_types`
 --
 ALTER TABLE `lessons`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `group_id` (`group_id`),
-  ADD KEY `subject_id` (`subject_id`);
+  ADD KEY `group_id` (`group_id`);
 
 --
 -- Индексы таблицы `migration`
@@ -374,15 +403,14 @@ ALTER TABLE `migration`
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `students_ibfk_1` (`group_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
 -- Индексы таблицы `students_in_group`
 --
 ALTER TABLE `students_in_group`
-  ADD KEY `group_id` (`group_id`),
-  ADD KEY `student_id` (`student_id`);
+  ADD PRIMARY KEY (`student_id`,`group_id`),
+  ADD KEY `students_in_group_ibfk_1` (`group_id`);
 
 --
 -- Индексы таблицы `students_in_lesson`
@@ -401,8 +429,7 @@ ALTER TABLE `subjects`
 -- Индексы таблицы `teachers`
 --
 ALTER TABLE `teachers`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `teachers_ibfk_1` (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `test_ege`
@@ -416,8 +443,7 @@ ALTER TABLE `test_ege`
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_role` (`user_role`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `user_roles`
@@ -438,7 +464,7 @@ ALTER TABLE `buildings`
 -- AUTO_INCREMENT для таблицы `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT для таблицы `group_types`
 --
@@ -448,12 +474,12 @@ ALTER TABLE `group_types`
 -- AUTO_INCREMENT для таблицы `lessons`
 --
 ALTER TABLE `lessons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT для таблицы `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT для таблицы `subjects`
 --
@@ -463,7 +489,7 @@ ALTER TABLE `subjects`
 -- AUTO_INCREMENT для таблицы `teachers`
 --
 ALTER TABLE `teachers`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT для таблицы `test_ege`
 --
@@ -473,7 +499,7 @@ ALTER TABLE `test_ege`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT для таблицы `user_roles`
 --
@@ -488,7 +514,6 @@ ALTER TABLE `user_roles`
 --
 ALTER TABLE `groups`
   ADD CONSTRAINT `building_ibfk_1` FOREIGN KEY (`building_id`) REFERENCES `buildings` (`id`),
-  ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`),
   ADD CONSTRAINT `grouptype_ibfk_1` FOREIGN KEY (`group_type_id`) REFERENCES `group_types` (`id`),
   ADD CONSTRAINT `subjects_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`);
 
@@ -496,8 +521,7 @@ ALTER TABLE `groups`
 -- Ограничения внешнего ключа таблицы `lessons`
 --
 ALTER TABLE `lessons`
-  ADD CONSTRAINT `lessons_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`),
-  ADD CONSTRAINT `lessons_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`);
+  ADD CONSTRAINT `lessons_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `students`
@@ -509,21 +533,15 @@ ALTER TABLE `students`
 -- Ограничения внешнего ключа таблицы `students_in_group`
 --
 ALTER TABLE `students_in_group`
-  ADD CONSTRAINT `students_in_group_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`),
-  ADD CONSTRAINT `students_in_group_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`);
+  ADD CONSTRAINT `students_in_group_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `students_in_group_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `students_in_lesson`
 --
 ALTER TABLE `students_in_lesson`
-  ADD CONSTRAINT `students_in_lesson_ibfk_1` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`id`),
-  ADD CONSTRAINT `students_in_lesson_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `teachers`
---
-ALTER TABLE `teachers`
-  ADD CONSTRAINT `teachers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `students_in_lesson_ibfk_1` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `students_in_lesson_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `test_ege`
@@ -531,12 +549,6 @@ ALTER TABLE `teachers`
 ALTER TABLE `test_ege`
   ADD CONSTRAINT `test_ege_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
   ADD CONSTRAINT `test_ege_ibfk_2` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_role`) REFERENCES `user_roles` (`id_user_role`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
