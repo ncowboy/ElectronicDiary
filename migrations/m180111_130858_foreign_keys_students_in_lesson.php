5bab99503016e465d4f4d0cf2998a2d88a0167f1
ncowboy <latentpickuper@gmail.com>
2018-01-16 17:40:58 +0300

    Поправил миграцию

--- migrations/m180111_130858_foreign_keys_students_in_lesson.php
+++ migrations/m180111_130858_foreign_keys_students_in_lesson.php
@@ -9,8 +9,6 @@ class m180111_130858_foreign_keys_students_in_lesson extends Migration
     public function safeUp()
     {
      $this->addPrimaryKey('student', 'students_in_lesson', ['student_id', 'lesson_id']);
-     $this->addForeignKey('students_in_lesson_ibfk_1', 'students_in_lesson', 'lesson_id', 'lessons', 'id', $delete = 'CASCADE', $update = 'CASCADE');
-     $this->addForeignKey('students_in_lesson_ibfk_2', 'students_in_lesson', 'student_id', 'students', 'id', $delete = 'CASCADE', $update = 'CASCADE'); 
     }
 
     public function safeDown()
