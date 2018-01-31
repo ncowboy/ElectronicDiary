b3a8eb82e126186a01490654b23b063297ad1154
ncowboy <latentpickuper@gmail.com>
2018-01-31 18:13:53 +0300

    Поправил модель и Readme

--- models/MyAccessControl.php
+++ models/MyAccessControl.php
@@ -10,7 +10,7 @@ namespace app\models;
 use Yii;
 use yii\filters\AccessControl;
 use yii\web\ForbiddenHttpException;
-use yii\web\User;
+
 
 /**
  * AccessControl provides simple access control based on a set of rules.
