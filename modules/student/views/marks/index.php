8591e475cb870f854a78ded3e620b278ff93a83d
ncowboy <latentpickuper@gmail.com>
2018-02-04 16:40:01 +0300

    добавил кнопку

--- modules/student/views/marks/index.php
+++ modules/student/views/marks/index.php
@@ -9,12 +9,16 @@ $this->params['breadcrumbs'][] = ['label' => 'Оценки'];
 <div class="my-marks">
  <h1><?= Html::encode($this->title) ?></h1>
  <br>
+ <?=Html::button(Html::tag('span', '', ['class' => 'fa fa-table']) . ' Экспорт в excel', [
+     'class' => 'btn btn-success']);?>
+ 
 
 <?= ListView::widget([
   'dataProvider' => $dataProvider,
   'itemView' => '_group',
   'summary' => false
 ]);?>
+ 
 </div>
 
 
