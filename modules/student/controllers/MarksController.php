18f75f409eca09301d123443f381e1e18bb151ba
ncowboy <latentpickuper@gmail.com>
2018-02-05 17:38:02 +0300

    Переделал отчет об успеваемости студента и добавил экспорт в файл.

--- modules/student/controllers/MarksController.php
+++ modules/student/controllers/MarksController.php
@@ -3,10 +3,11 @@
 namespace app\modules\student\controllers;
 
 use yii\web\Controller;
-use app\models\StudentsInGroup;
 use app\models\Students;
 use yii\data\ActiveDataProvider;
 use app\models\Users;
+
+
 
 class MarksController extends Controller
 {
@@ -18,7 +19,8 @@ class MarksController extends Controller
       $userFullName = $user->userFullName;
       $studentId = Students::findOne(['user_id' => $userId]);
       $dataProvider = new ActiveDataProvider([
-          'query' => StudentsInGroup::find()->where(['student_id' => $studentId ]),
+          'query' => \app\models\StudentsInLesson::find()->where(['student_id' => $studentId ]),
+          'sort' => false
         ]);
       
       return $this->render('index', [
--- modules/student/views/marks/index.php
+++ modules/student/views/marks/index.php
@@ -1,23 +1,133 @@
 <?php
-use yii\widgets\ListView;
-use yii\helpers\Html;
 
-$this->title = 'Оценки ученика: ' . $user;
+use yii\helpers\Html;
+use kartik\grid\GridView;
+use kartik\export\ExportMenu;
+
+$this->title = 'Успеваемость ученика';
 $this->params['breadcrumbs'][] = ['label' => 'Оценки'];
 ?>
 
 <div class="my-marks">
  <h1><?= Html::encode($this->title) ?></h1>
  <br>
- <?=Html::button(Html::tag('span', '', ['class' => 'fa fa-table']) . ' Экспорт в excel', [
-     'class' => 'btn btn-success']);?>
- 
+<?php
+  $export = ExportMenu::widget([
+    'dataProvider' => $dataProvider,
+    'target' => '_self',
+    'krajeeDialogSettings' => [ 'useNative'=>true ],
+    'showConfirmAlert' => false,
+    'showColumnSelector' => false,
+    'filename' => 'marks',
+    'dropdownOptions' => [
+      'icon' => '<i class="fa fa-share-square-o"></i>',
+      'label' => ' Экспорт',
+      'class' => 'btn btn-default'
+    ],
+    'columns'=>[
+      [
+            'attribute' => 'marksGroupedString',
+            'group'=>true,  
+            'groupedRow'=>true,                    
+            'groupOddCssClass'=>'kv-grouped-row',  
+            'groupEvenCssClass'=>'kv-grouped-row', 
+      ],
+      'dateTime',
+      'theme',
+     [
+       'attribute'=>'attendance',
+       'content' => function($model){
+        if ($model->attendance==1) {
+          return 'Присутств.';
+        }else{
+          return 'Отсутств.';
+        }
+       }
+      ], 
+      [
+       'attribute'=>'mark_work_at_lesson',
+       'contentOptions' => [
+                'class'=>'mark-cell-style'
+            ]
+      ], 
+       [
+       'attribute'=>'mark_homework',
+       'contentOptions' => [
+                'class'=>'mark-cell-style'
+            ]
+      ], 
+       [
+       'attribute'=>'mark_dictation',
+       'contentOptions' => [
+                'class'=>'mark-cell-style'
+            ]
+      ], 
+      'comment'
+      ]
+    
+  ]);
+?>
 
-<?= ListView::widget([
-  'dataProvider' => $dataProvider,
-  'itemView' => '_group',
-  'summary' => false
-]);?>
+ 
+ <?=GridView::widget([
+    'dataProvider'=>$dataProvider,
+    'showPageSummary'=>true,
+    'pjax'=>true,
+    'striped'=>false,
+    'krajeeDialogSettings' => [ 'useNative'=>true ],
+    'formatter' => ['class' => 'yii\i18n\Formatter','nullDisplay' => 'б/о'],
+    'summary' => "Показано с <strong>{begin}</strong> по <strong>{end}</strong> из <strong>{totalCount}</strong>",
+    'showPageSummary' => $pageSummary,
+    'panel'=>[
+      'type'=>'primary', 
+      'heading'=> $user,
+      'footer' => false
+      ],
+    'toolbar' =>  [
+        $export
+    ],
+    // set export properties
+  
+    'columns'=>[
+      [
+            'attribute' => 'marksGroupedString',
+            'group'=>true,  
+            'groupedRow'=>true,                    
+            'groupOddCssClass'=>'kv-grouped-row',  
+            'groupEvenCssClass'=>'kv-grouped-row', 
+      ],
+      'dateTime',
+      'theme',
+     [
+       'class'=>'kartik\grid\BooleanColumn',
+       'attribute'=>'attendance',
+       'trueLabel' => 'Присутств.',
+       'falseLabel' => 'Отсутств.',
+       'contentOptions' => [
+                'class'=>'mark-cell-style'
+            ]
+      ], 
+      [
+       'attribute'=>'mark_work_at_lesson',
+       'contentOptions' => [
+                'class'=>'mark-cell-style'
+            ]
+      ], 
+       [
+       'attribute'=>'mark_homework',
+       'contentOptions' => [
+                'class'=>'mark-cell-style'
+            ]
+      ], 
+       [
+       'attribute'=>'mark_dictation',
+       'contentOptions' => [
+                'class'=>'mark-cell-style'
+            ]
+      ], 
+      'comment'
+      ]
+ ]);?>
  
 </div>
 
