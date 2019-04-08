d79d642567b743ffc92018f5fc0f86c37f6d1e0c
Igor Borisov <latentpickuper@gmail.com>
2019-04-08 18:27:39 +0300

    ашÐfixed

--- models/StudentsInGroup.php
+++ models/StudentsInGroup.php
@@ -18,131 +18,139 @@ use yii\helpers\ArrayHelper;
  */
 class StudentsInGroup extends \yii\db\ActiveRecord
 {
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
-    {
-        return 'students_in_group';
-    }
+  /**
+   * @inheritdoc
+   */
+  public static function tableName()
+  {
+    return 'students_in_group';
+  }
 
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['student_id', 'group_id'], 'required'],
-            [['student_id', 'group_id'], 'integer'],
-            [['student_id'], 'unique', 'targetAttribute' => ['student_id', 'group_id']],
-            [['group_id'], 'unique', 'targetAttribute' => ['group_id', 'student_id']],
-            [['group_id'], 'exist', 'skipOnError' => true, 'targetClass' => Groups::className(), 'targetAttribute' => ['group_id' => 'id']],
-            [['student_id'], 'exist', 'skipOnError' => true, 'targetClass' => Students::className(), 'targetAttribute' => ['student_id' => 'id']],
-        ];
-    }
+  /**
+   * @inheritdoc
+   */
+  public function rules()
+  {
+    return [
+      [['student_id', 'group_id'], 'required'],
+      [['student_id', 'group_id'], 'integer'],
+      [['student_id'], 'unique', 'targetAttribute' => ['student_id', 'group_id']],
+      [['group_id'], 'unique', 'targetAttribute' => ['group_id', 'student_id']],
+      [['group_id'], 'exist', 'skipOnError' => true, 'targetClass' => Groups::className(), 'targetAttribute' => ['group_id' => 'id']],
+      [['student_id'], 'exist', 'skipOnError' => true, 'targetClass' => Students::className(), 'targetAttribute' => ['student_id' => 'id']],
+    ];
+  }
 
-    /**
-     * @inheritdoc
-     */
-    public function attributeLabels()
-    {
-        return [
-            'student_id' => 'Student ID',
-            'group_id' => 'Group ID',
-            'studentFullName' => 'ФИО',
-            'studentPhone' => 'Телефон',
-            'studentEmail' => 'Email'
-        ];
-    }
+  /**
+   * @inheritdoc
+   */
+  public function attributeLabels()
+  {
+    return [
+      'student_id' => 'Student ID',
+      'group_id' => 'Group ID',
+      'studentFullName' => 'ФИО',
+      'studentPhone' => 'Телефон',
+      'studentEmail' => 'Email'
+    ];
+  }
 
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getGroup()
-    {
-        return $this->hasOne(Groups::className(), ['id' => 'group_id']);
-    }
-    
-    public function getGroupCode()
-    {
-        return $this->group->getGroupCode();
-    }
+  /**
+   * @return \yii\db\ActiveQuery
+   */
+  public function getGroup()
+  {
+    return $this->hasOne(Groups::className(), ['id' => 'group_id']);
+  }
 
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getStudent()
-    {
-        return $this->hasOne(Students::className(), ['id' => 'student_id']);
-    }
+  public function getGroupCode()
+  {
+    return $this->group->getGroupCode();
+  }
 
-    public function getUser(){
-      return $this->hasOne(Users::className(), ['id' => $this->student->user_id]);
-    }
-    
-    public function getStudentFullName()
-    {
-        return $this->student->user->userFullName;
-    }
-    
-    public function getStudentPhone()
-    {
-        return $this->student->phone_number;
-    }
-    
-    public function getStudentEmail()
-    {
-        return $this->student->user->email;
-    }
-    
-    public function getStudentGroupsList(){
-        return $this->hasMany(Students::className(), ['student_id' => 'id'])
-            ->viaTable('students_in_group', ['group_id' => 'id']);
-    }
-      //При добавлении ученика в группу добавляем его во все уроки группы
-    public function afterSave($insert, $changedAttributes) {
-        parent::afterSave($insert, $changedAttributes);
-        if ($insert) {
-            $studentCreateDate = $this->student->user->created_at;
-            $lessons = Lessons::find()->where(['group_id' => $this->group_id])
-                ->andWhere("datetime > '$studentCreateDate'")->all();
-            $lessonsArr = ArrayHelper::toArray($lessons, [
-                'app\models\Lessons' => ['id']
-            ]);
-            $lessonsIds = [];
-            foreach ($lessonsArr as $value) {
-                array_push($lessonsIds, $value['id']);
-            }
-            if(isset($lessonsIds)){
-                foreach ($lessonsIds as $value) {
-                    $model = new StudentsInLesson();
-                    $model->student_id = $this->student_id;
-                    $model->lesson_id = $value;
-                    $model->save();
-                }
-            }
-        }   
-    }
-     //При удалении ученика из группы удаляем его изо всех уроков группы
-    public function beforeDelete() {
-        parent::beforeDelete();
-        $lessons = Lessons::findAll(['group_id' => $this->group_id]);
-        $lessonsArr = ArrayHelper::toArray($lessons, [
+  /**
+   * @return \yii\db\ActiveQuery
+   */
+  public function getStudent()
+  {
+    return $this->hasOne(Students::className(), ['id' => 'student_id']);
+  }
+
+  public function getUser()
+  {
+    return $this->hasOne(Users::className(), ['id' => $this->student->user_id]);
+  }
+
+  public function getStudentFullName()
+  {
+    return $this->student->user->userFullName;
+  }
+
+  public function getStudentPhone()
+  {
+    return $this->student->phone_number;
+  }
+
+  public function getStudentEmail()
+  {
+    return $this->student->user->email;
+  }
+
+  public function getStudentGroupsList()
+  {
+    return $this->hasMany(Students::className(), ['student_id' => 'id'])
+      ->viaTable('students_in_group', ['group_id' => 'id']);
+  }
+
+  //При добавлении ученика в группу добавляем его во все уроки группы
+  public function afterSave($insert, $changedAttributes)
+  {
+    parent::afterSave($insert, $changedAttributes);
+    if ($insert) {
+      $studentCreateDate = $this->student->user->created_at;
+      $lessons = Lessons::find()->where(['group_id' => $this->group_id])
+        ->andWhere("datetime > '$studentCreateDate'")->all();
+      $lessonsArr = ArrayHelper::toArray($lessons, [
         'app\models\Lessons' => ['id']
-        ]);
-        $lessonsIds = [];
-        foreach ($lessonsArr as $value) {
+      ]);
+      $lessonsIds = [];
+      foreach ($lessonsArr as $value) {
         array_push($lessonsIds, $value['id']);
+      }
+      if (isset($lessonsIds)) {
+        foreach ($lessonsIds as $value) {
+          $model = new StudentsInLesson();
+          $model->student_id = $this->student_id;
+          $model->lesson_id = $value;
+          $model->save();
         }
-        if(isset($lessonsIds)){
-            foreach ($lessonsIds as $value) {
-            $model = StudentsInLesson::findOne(['student_id' => $this->student_id, 'lesson_id' => $value]);
-            $model->delete();
-                }
-            }
-            return TRUE;
-        
+      }
+    }
+  }
+
+  //При удалении ученика из группы удаляем его изо всех уроков группы
+  public function beforeDelete()
+  {
+    parent::beforeDelete();
+    $lessons = Lessons::findAll(['group_id' => $this->group_id]);
+    $lessonsArr = ArrayHelper::toArray($lessons, [
+      'app\models\Lessons' => ['id']
+    ]);
+    $lessonsIds = [];
+    foreach ($lessonsArr as $value) {
+      array_push($lessonsIds, $value['id']);
+    }
+    if (isset($lessonsIds)) {
+      foreach ($lessonsIds as $value) {
+        $model = StudentsInLesson::findOne(['student_id' => $this->student_id, 'lesson_id' => $value]);
+        if (!is_null($model)) {
+          $model->delete();
         }
+      }
+    }
+    return TRUE;
+
+  }
 }
 
 
